d34efd5e0dc57932127317a67fc5c1f61cb816cc
Chao-Jen Wong <cwon2@HB-Chao-Jens-MacBook-Pro-Tapscott.local>
2020-03-16 09:52:29 -0700

    add tools.R

---
+++ scripts/tools.R
@@ -0,0 +1,11 @@
+# lm_eqn is a copy from https://groups.google.com/forum/#!topic/ggplot2/1TgH-kG5XMA
+lm_eqn <- function(df){
+    m <- lm(y ~ x, df);
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)~"="~r2, 
+         list(a = format(unname(coef(m)[1]), digits = 2),
+              b = format(unname(coef(m)[2]), digits = 2),
+              r2 = format(sqrt(summary(m)$r.squared), digits = 3)))
+    as.character(as.expression(eq));
+}
+
+do_goset <- funciton(universe, selected) {}
